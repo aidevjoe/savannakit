cdf9d0dda7374e1d7f889dd4087fb550ccdf5e0e
Joe <aidevjoe@gmail.com>
2020-11-08 03:11:02 +0800

    Update String+Range.swift

--- Sources/SavannaKit/Util/String+Range.swift
+++ Sources/SavannaKit/Util/String+Range.swift
@@ -11,7 +11,21 @@ import Foundation
 extension String {
 	
 	func nsRange(fromRange range: Range<Index>) -> NSRange {
-		return NSRange(range, in: self)
+		return Foundation.NSRange(range, in: self)
 	}
+
+    func NSRange(of substring: String) -> NSRange? {
+        // Get the swift range
+        guard let range = range(of: substring) else { return nil }
+
+        // Get the distance to the start of the substring
+        let start = distance(from: startIndex, to: range.lowerBound) as Int
+        //Get the distance to the end of the substring
+        let end = distance(from: startIndex, to: range.upperBound) as Int
+
+        //length = endOfSubstring - startOfSubstring
+        //start = startOfSubstring
+        return NSMakeRange(start, end - start)
+    }
 
 }
